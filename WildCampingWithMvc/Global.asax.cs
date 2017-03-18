201c9b4c0c412dab682c2ec545c6466de38ec331
Todor Arabadzhiev <todor_ia@abv.bg>
2017-03-17 23:48:43 -0700

    nazdrave

--- WildCampingWithMvc/Global.asax.cs
+++ WildCampingWithMvc/Global.asax.cs
@@ -9,6 +9,8 @@ namespace WildCampingWithMvc
     {
         protected void Application_Start()
         {
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
